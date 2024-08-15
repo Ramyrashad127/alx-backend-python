ccf71df3267d45cd331e9b21845741aee3ae6907
Ramyrashad127 <ramy41599@gmail.com>
2024-08-15 13:14:40 +0300

    new

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+no modules
+"""
+from typing import List, Tuple, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """function"""
+    return [(i, len(i)) for i in lst]
