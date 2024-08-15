21a179b94d0900b9c4d22b98810bc656c7bd3eac
Ramyrashad127 <ramy41599@gmail.com>
2024-08-15 11:06:03 +0300

    new

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+modules
+"""
+import math
+
+
+def floor(a: float) -> int:
+    """floor function"""
+    return math.floor(a)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+no modules
+"""
+
+
+def to_str(a: float) -> str:
+    """ function"""
+    return str(a)
