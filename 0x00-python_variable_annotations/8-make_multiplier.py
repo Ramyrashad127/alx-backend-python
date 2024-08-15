3d8115c090ac6e76165c791e44fc8110aa9802c6
Ramyrashad127 <ramy41599@gmail.com>
2024-08-15 13:44:26 +0300

    new

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,9 +5,9 @@ no modules
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """function"""
+    """new function for python"""
     def multiply_by_multiplier(value: float) -> float:
-        """function"""
+        """new function for python"""
         return value * multiplier
 
     return multiply_by_multiplier
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,5 +6,5 @@ from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """function"""
+    """new function for python"""
     return [(i, len(i)) for i in lst]
