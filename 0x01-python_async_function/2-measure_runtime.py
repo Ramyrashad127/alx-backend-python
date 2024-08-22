a6bedd6a82cf0a242292c8eb07837e19d23a1530
Ramyrashad127 <ramy41599@gmail.com>
2024-08-23 00:22:34 +0300

    new

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,8 +6,8 @@ wait_n = __import__('2-measure_runtime.py').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Waits for delays"""
-    start = time.time()
+    """new function for python"""
+    st = time.time()
     asyncio.run(wait_n(n, max_delay))
-    total = time.time() - start
-    return total / n
+    total_time = time.time() - st
+    return total_time / n
