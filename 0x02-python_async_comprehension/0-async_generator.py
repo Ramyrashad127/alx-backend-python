7236a439a536f6faf83ad06a1932232eed706003
Ramyrashad127 <ramy41599@gmail.com>
2024-08-28 14:38:37 +0300

    new

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""import modules"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[int, None]:
+    """async generator for integers numbers"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        v = random.uniform(0, 10)
+        yield v
